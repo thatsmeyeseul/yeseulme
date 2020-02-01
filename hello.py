c69d63dbe29691bf3899208bb370f54a451dbe12
yeseul <sessilia93@gmail.com>
2020-02-01 16:48:56 +0900

    host, port arguments parsing

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+import argparse
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,6 +7,21 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+
+def parse_args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-i', '--interface',
+			type=str,
+			default='0.0.0.0',
+			help='Local interface to listen at')
+	parser.add_argument('-p', '--port',
+			type=int,
+			default=5000,
+			help='The port to bind the server to')
+	return parser.parse_args()
+
+
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', debug=True)
+	args = parse_args()
+	app.run(host=args.interface, port=args.port, debug=True)
 
